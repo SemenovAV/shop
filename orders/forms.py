0b6f2d4eff9165ac2a841a32e1088c4098980c8c
Aleksey Semenov <7.on.off@bk.ru>
2020-06-21 22:37:09 +0300

    fix order app

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+
 from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['user']
+        exclude = ('user', 'created', 'updated', 'paid')
--- orders/views.py
+++ orders/views.py
@@ -1,25 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
+
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
 from orders.models import OrderItem
+from users.models import CustomUser
 
 
-def order_create(request):
-    cart = Cart(request)
-    if request.method == 'POST':
-        form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            for item in cart:
-                OrderItem.objects.create(order=order,
-                                         product=item['product'],
-                                         price=item['price'],
-                                         quantity=item['quantity'])
-            # очистка корзины
-            cart.clear()
-            return render(request, 'orders/order/created.html',
-                          {'order': order})
-    else:
-        form = OrderCreateForm
-    return render(request, 'orders/order/create.html',
-                  {'cart': cart, 'form': form})
+class OrderCreate(CreateView):
+    template_name = 'orders/order/create.html'
+    form_class = OrderCreateForm
+    request = None
+    args = None
+    kwargs = None
+    user = None
+    cart = None
+
+    def setup(self, request, *args, **kwargs):
+        self.request = request
+        self.args = args
+        self.kwargs = kwargs
+        self.user = request.user
+        self.cart = Cart(request)
+        self.extra_context = {
+            'cart': self.cart
+        }
+
+    def get_success_url(self):
+        return reverse('orders:order_ok')
+
+    @login_required()
+    def form_valid(self, form):
+        order = form.save(commit=False)
+        order.user = CustomUser.objects.get(pk=self.request.user.pk)
+        order.save()
+        order = form.save()
+        for item in self.cart:
+            OrderItem.objects.create(order=order,
+                                     product=item['product'],
+                                     price=item['price'],
+                                     quantity=item['quantity'])
+        self.cart.clear()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class OrderOk(TemplateView):
+    template_name = 'orders/order/created.html'
