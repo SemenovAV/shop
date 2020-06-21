432b1781d26a6d927e677e970ef6c81737becfff
Aleksey Semenov <7.on.off@bk.ru>
2020-06-21 08:14:15 +0300

    adds model creations to orders app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from storefront.models import Product
+from users.models import CustomUser
+
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='orders', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
